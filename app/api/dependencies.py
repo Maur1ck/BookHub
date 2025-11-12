b51681e40f63d1a6f6b92ac160fb753db808173f
comander <misha20061508@yandex.ru>
2025-11-12 18:55:13 +0300

    fix: add async for with

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -6,8 +6,8 @@ from app.database import async_session_maker
 from app.utils.db_manager import DBManager
 
 
-def get_db():
-    with DBManager(session_factory=async_session_maker) as db:
+async def get_db():
+    async with DBManager(session_factory=async_session_maker) as db:
         yield db
 
 DBDep = Annotated[DBManager, Depends(get_db)]
