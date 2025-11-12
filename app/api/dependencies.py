dbedd5ce6f0cb3348396f94a704cf7791b46a25f
comander <misha20061508@yandex.ru>
2025-11-12 18:47:28 +0300

    feat: add DBManager and DBDep

---
+++ app/api/dependencies.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from app.database import async_session_maker
+from app.utils.db_manager import DBManager
+
+
+def get_db():
+    with DBManager(session_factory=async_session_maker) as db:
+        yield db
+
+DBDep = Annotated[DBManager, Depends(get_db)]
